gbraad-devenv/gitpod-template-ubuntu

Initial version

Containerfile
@@ -1,0 +1,16 @@
+FROM ubuntu:latest
+
+USER root
+RUN apt-get update && apt-get install -yq \
+    git \
+    git-lfs \
+    sudo \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/*
+
+RUN useradd -l -u 33333 -G sudo -md /home/gitpod -s /bin/bash -p gitpod gitpod
+
+# Install user environment
+CMD /bin/bash -l
+USER gitpod
+ENV USER gitpod
+WORKDIR /home/gitpod

gbraad-devenv/gitpod-template-debian

Containerfile
@@ -1,0 +1,16 @@
+FROM debian:latest
+
+USER root
+RUN apt-get update && apt-get install -yq \
+    git \
+    git-lfs \
+    sudo \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/*
+
+RUN useradd -l -u 33333 -G sudo -md /home/gitpod -s /bin/bash -p gitpod gitpod
+
+# Install user environment
+CMD /bin/bash -l
+USER gitpod
+ENV USER gitpod
+WORKDIR /home/gitpod